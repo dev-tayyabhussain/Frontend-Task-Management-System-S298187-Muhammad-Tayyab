7740e783441aa4f6dfc787bdf7cfebdf9c7e249e
dev-tayyabhussain <devtayyabhussain@gmail.com>
2025-03-01 19:04:04 +0500

    add navigation routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import TaskList from './components/TaskList';
+import ProjectList from './components/ProjectList';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import TaskForm from './components/TaskForm';
+import EditTask from './components/EditTask';
 
 function App() {
   return (
-    <></>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tasks"
+              element={
+                <PrivateRoute>
+                  <TaskList />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/projects"
+              element={
+                <PrivateRoute>
+                  <ProjectList />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/task/new"
+              element={
+                <PrivateRoute>
+                  <TaskForm />
+                </PrivateRoute>
+              }
+            />
+
+            <Route path="/task/edit/:id"
+              element={
+                <PrivateRoute>
+                  <EditTask />
+                </PrivateRoute>
+              }
+            />
+
+          </Routes>
+
+
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
